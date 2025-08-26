aba20db9c269ddfdf1f95d6788e99e82d6567ee4
BSL95 <brettsl95@gmail.com>
2025-08-25 20:58:51 -0400

    RandomPassword.ps1

--- RandomPassword.ps1
+++ RandomPassword.ps1
@@ -1,13 +1,26 @@
 function RandomPassword {
-    $length = 15
-    $charset += [char[]]"abcdefghijklmnopqrstuvwxyz"
-    $charset += [char[]]"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    $charset += [char[]]"0123456789"
-    $charset += [char[]]"!@#$%^&*()-_=+[]{};:<>,.?/\|`~"
+    param([int]$Length = 15)
 
-    $password = -join (1..$length | ForEach-Object { $charset | Get-Random })
-    return $password
+    $lower  = "abcdefghijklmnopqrstuvwxyz"
+    $upper  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    $digit  = "0123456789"
+    $symbol = "!@#$%^&*()-_=+[]{};:<>,.?/\|`~"
+
+    # Pick at least one from each
+    $password = @(
+        ($lower  | Get-Random -Count 1)
+        ($upper  | Get-Random -Count 1)
+        ($digit  | Get-Random -Count 1)
+        ($symbol | Get-Random -Count 1)
+    )
+
+    # Fill the rest from the full pool
+    $allChars = ($lower + $upper + $digit + $symbol).ToCharArray()
+    $password += (1..($Length-4) | ForEach-Object { $allChars | Get-Random })
+
+    # Shuffle so it’s not predictable
+    -join ($password | Sort-Object {Get-Random})
 }
 
-# Call the function
-RandomPassword
+# Calling the function
+RandomPassword     
